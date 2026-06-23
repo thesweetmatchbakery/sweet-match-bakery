16cfb2e42698dae50acae42b051f88341b30f478
thesweetmatchbakery <thesweetmatchbakery@gmail.com>
2026-06-23 16:20:43 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sweetmatchbakery.app',
+  appName: 'Sweet Match Bakery',
+  webDir: 'www'
+};
+
+export default config;
